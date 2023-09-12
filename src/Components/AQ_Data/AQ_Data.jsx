f90d8c54a65c53c08ab3a3294e24e732af41be23
Jiffy <120106650+JiffyLTD@users.noreply.github.com>
2023-09-12 16:14:20 +0300

    добавлено получение данных с сервера

--- src/Components/AQ_Data/AQ_Data.jsx
+++ src/Components/AQ_Data/AQ_Data.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React from "react";
 
-const AQ_Data = ({aq_data}) => {
+const AQ_Data = ({ aq_data }) => {
+  const formatDate = (str) => {
+    // Создать объект Date из строки с помощью конструктора
+    var date = new Date(str);
+
+    // Добавить ведущий ноль к числу, если он меньше 10
+    function pad(n) {
+      return n < 10 ? "0" + n : n;
+    }
+
+    // Получить компоненты даты и времени из объекта Date
+    var day = pad(date.getDate());
+    var month = pad(date.getMonth() + 1); // Месяцы начинаются с 0
+    var year = date.getFullYear();
+    var hour = pad(date.getHours());
+    var minute = pad(date.getMinutes());
+    var second = pad(date.getSeconds());
+
+    // Соединить компоненты в одну строку с разделителями
+    return (
+      day + "." + month + "." + year + " " + hour + ":" + minute + ":" + second
+    );
+  };
+
   return (
-    <div className='px-2 py-2 mb-4' style={{background: '#00183A'}}>
-        <h4 className='text-light'>Дата и время замера: {aq_data.creationDate}</h4>
-        <p className='text-light fs-4'>Температура: {aq_data.temperature} °C</p>
-        <p className='text-light fs-4'>Влажность: {aq_data.humidity} %</p>
+    <div className="px-2 py-2 mb-4" style={{ background: "#00183A" }}>
+      <h4 className="text-light">
+        Дата и время замера: {formatDate(aq_data.creationDate)}
+      </h4>
+      <p className="text-light fs-4">Температура: {aq_data.temperature} °C</p>
+      <p className="text-light fs-4">Влажность: {aq_data.humidity} %</p>
     </div>
-  )
-}
+  );
+};
 
-export default AQ_Data
+export default AQ_Data;
--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,30 +1,41 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Container, Spinner } from "react-bootstrap";
 import AQ_Data from "../Components/AQ_Data/AQ_Data";
+import { ApiService } from "../Components/API/ApiService";
 
 const Main = () => {
-  const data = [
-    {
-      id: 1,
-      creationDate: "02.02.2023 18:34:43",
-      temperature: 25.5,
-      humidity: 45.4,
-    },
-    {
-        id: 2,
-        creationDate: "02.02.2023 18:35:43",
-        temperature: 26.5,
-        humidity: 47.4,
-      },
-  ];
+  const [wsData, setWsData] = useState([]);
+  const [isLoad, setIsLoad] = useState(false);
+  const [networkError, setNetworkError] = useState('');
+
+  const fetchData = async () => {
+    setIsLoad(true);
+    try {
+      let response = await ApiService.getAll();
+      setWsData(response.data.dtoModel);
+    } catch (error) {
+      setNetworkError(error.message);
+    }
+    setIsLoad(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <Container className="mt-5 py-2" style={{ background: "#053566" }}>
       <h3 className="text-light">Главная страница</h3>
       <hr className="text-light" />
       <Container className="py-2">
-        {data !== null ? (
-          data.map((data) => <AQ_Data key={data.id} aq_data={data} />)
+        {isLoad ? (
+          <div className="text-center">
+            <Spinner animation="border" variant="light" />
+            <br />
+            <span className="text-danger">{networkError}</span>
+          </div>
+        ) : wsData !== null ? (
+          wsData.map((data) => <AQ_Data key={data.id} aq_data={data} />)
         ) : (
           <div className="px-2 py-2" style={{ background: "#00183A" }}>
             <h4 className="text-light">Замеров еще не происходило</h4>
