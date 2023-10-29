4efc47ff1e612f0a5bf5c64a7e3a2d0465ba630d
Jiffy <120106650+JiffyLTD@users.noreply.github.com>
2023-10-29 18:14:27 +0300

    добалено отображение данных с новых датчиков

--- src/models/IWSData.ts
+++ src/models/IWSData.ts
@@ -4,5 +4,9 @@ export interface IWSData{
     id: Guid,
     temperature: number,
     humidity: number,
+    pm_1: number,
+    pm2_5: number,
+    pm_10: number,
+    co: number,
     creationDate: Date
 }
